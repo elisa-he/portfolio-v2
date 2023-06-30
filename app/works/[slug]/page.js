9e83cb378b12e2d3da3a61b1d6a22c3757809d8a
elisa-he <he.elisa1998@gmail.com>
2023-06-30 02:54:26 -0700

    information for works page added

--- app/works/[slug]/page.js
+++ app/works/[slug]/page.js
@@ -3,30 +3,52 @@ import logo from "public/eh.svg";
 import works from "/json/work.json";
 import Header from "/app/components/header";
 import Footer from "/app/components/footer";
-import Accordion from "/app/components/accordion";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 export default function Page({ params }) {
   const workId = params.slug;
 
-  const title = works?.length && works[workId]?.title;
+  // from the array of objects, find the object key title value the same as the slug, then declare variables as per object gotten.
+
+  const work = works.findIndex((obj) => obj.title == workId);
+
+  const title = works?.length && works[work]?.title;
 
   const skills =
     works?.length &&
-    works[workId]?.usedSkill.map((skill, i) => <li key={i}>{skill}</li>);
+    works[work]?.usedSkill.map((skill, i) => <li key={i}>{skill}</li>);
 
   const tools =
     works?.length &&
-    works[workId]?.usedTool.map((tool, i) => <li key={i}>{tool}</li>);
+    works[work]?.usedTool.map((tool, i) => <li key={i}>{tool}</li>);
 
-  const image = works?.length && works[workId]?.image;
+  const image = works?.length && works[work]?.image;
 
-  const link = works?.length && works?.title[workId].link;
+  const link = works?.length && works[work].link;
 
   const team =
-    works?.length && works[workId]?.team.map((t, i) => <li key={i}>{t}</li>);
+    works?.length && works[work]?.team.map((t, i) => <li key={i}>{t}</li>);
 
-  const projectDesc = works?.length && works[workId]?.desc;
+  const projectDesc = works?.length && works[work]?.desc;
+
+  const designDesc = works?.length && works[work]?.designDesc;
+  // const highlightDesc =
+  //   works?.length &&
+  //   works[work]?.highlights.map((h, i) => {
+  //     <p>{h.highlightDesc}</p>;
+  //   });
+  const learnedDesc = works?.length && works[work]?.learnedDesc;
+
+  const [isActive, setIsActive] = useState(0);
+
+  const handleClick = (key) => {
+    if (key === isActive) {
+      setIsActive(0);
+    } else {
+      setIsActive(key);
+    }
+  };
 
   return (
     <>
@@ -55,7 +77,42 @@ export default function Page({ params }) {
             <button className="uppercase">live site</button>
           </Link>
 
-          <Accordion />
+          <section>
+            <div className="tabs">
+              <div className="tab 1" onClick={() => handleClick(1)}>
+                <h3 key={1}>planning</h3>
+                <div>{isActive === 1 ? "-" : "+"}</div>
+              </div>
+              {isActive === 1 && <p className="desc">{designDesc}</p>}
+              <div className="tab 2" onClick={() => handleClick(2)}>
+                <h3 key={2}>highlights</h3>
+                <div>{isActive === 2 ? "-" : "+"}</div>
+              </div>
+              {isActive === 2 && (
+                <div className="desc">
+                  {works?.length &&
+                    works[work]?.highlights?.map((h) => {
+                      return (
+                        <div className="highlightWrap">
+                          <img
+                            className="highlightImg"
+                            src={h.highlightImg}
+                            alt=""
+                          />
+                          <p className="highlightDesc">{h.highlightDesc}</p>
+                        </div>
+                      );
+                    })}
+                </div>
+              )}
+              <div className="tab 3" onClick={() => handleClick(3)}>
+                <h3 key={3}>what i learned</h3>
+                <div>{isActive === 3 ? "-" : "+"}</div>
+              </div>
+
+              {isActive === 3 && <p className="desc">{learnedDesc}</p>}
+            </div>
+          </section>
 
           <div className="singleBtn">
             <Link href={`/work`} className="btnGreen ">
